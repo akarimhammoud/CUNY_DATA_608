40bb5b847b90e50effadf678b06fff433003d86f
Karim Hammoud <68577624+akarimhammoud@users.noreply.github.com>
2021-12-05 13:34:30 -0500

    kh update

--- module6/d3_lab/01answer.js
+++ module6/d3_lab/01answer.js
@@ -1,8 +1,36 @@
 d3.csv('ue_industry.csv', data => {
 
-    // Define your scales and generator here.
+    console.log(data);
 
-    d3.select('#answer1')
-        // append more elements here
+    const xScale = d3.scaleLinear()
+        .domain([-totalYmax/2, totalYmax/2)
+        .range([500, 20]);
+
+    const fillScale = d3.scaleOrdinal()
+        .domain(indusries)
+        .range(colors);
+
+    const stackLayout = d3.stack()
+        .keys(indusries)
+        .offset(d3.stackOffsetSilhouette)
+
+    const stackArea = d3.area()
+        .x((d,i)) => {
+            console.log({"d": d, "i": i});
+            return xScale(i)
+
+        .y0(d => yScale(d[0]))
+        .y1(d => yScale(d[1]))
+        curve(d3.curveBasis);
+    
+        }
+
+
+    d3.select('#answer2')
+        .selectAll('path')
+        .daatq(stackLayout(data))
+    
+
+        
 
 });
